5a49cf62e64b5ae18e709b268617f7135db3472b
leiqiao <leiqiao0@aliyun.com>
2018-04-14 14:17:02 +0800

    create project basic structure

---
+++ app/src/main/java/com/lgsim/engine/graphEditor/app/Application.java
@@ -0,0 +1,15 @@
+package com.lgsim.engine.graphEditor.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application
+{
+  private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+
+  public static void main(String[] args)
+  {
+    log.debug("init {}", 42);
+  }
+}
