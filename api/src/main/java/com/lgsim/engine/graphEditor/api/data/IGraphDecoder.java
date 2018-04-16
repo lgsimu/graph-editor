d614b25653b34f69ae9ac33daf429186e43e3fc8
leiqiao <leiqiao0@aliyun.com>
2018-04-16 16:15:39 +0800

    modify graph encoder & decoder parameter type

--- api/src/main/java/com/lgsim/engine/graphEditor/api/data/IGraphDecoder.java
+++ api/src/main/java/com/lgsim/engine/graphEditor/api/data/IGraphDecoder.java
@@ -1,11 +1,11 @@
 package com.lgsim.engine.graphEditor.api.data;
 
-import java.io.InputStream;
+import org.jetbrains.annotations.NotNull;
 
 public interface IGraphDecoder
 {
   /**
    * 从输入数据流中解码出图
    */
-  IGraph decode(InputStream inputStream);
+  IGraph decode(@NotNull byte[] bytes);
 }
