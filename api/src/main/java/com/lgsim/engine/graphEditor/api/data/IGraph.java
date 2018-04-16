acd1f7f5d17fbdf9c323cd57a081efb681491e2e
leiqiao <leiqiao0@aliyun.com>
2018-04-16 13:36:15 +0800

    graph

--- api/src/main/java/com/lgsim/engine/graphEditor/api/data/IGraph.java
+++ api/src/main/java/com/lgsim/engine/graphEditor/api/data/IGraph.java
@@ -1,6 +1,11 @@
 package com.lgsim.engine.graphEditor.api.data;
 
+import java.util.Collection;
+
 public interface IGraph
 {
-
+  /**
+   * 获取图中所有的节点
+   */
+  Collection<IVertex> getAllVertexes();
 }
