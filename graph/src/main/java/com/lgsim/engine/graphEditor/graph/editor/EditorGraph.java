a13efc4aec3249a5ac16099bedcde297846ebff3
leiqiao <leiqiao0@aliyun.com>
2018-04-18 17:04:26 +0800

    implements IGraph api

--- graph/src/main/java/com/lgsim/engine/graphEditor/graph/editor/EditorGraph.java
+++ graph/src/main/java/com/lgsim/engine/graphEditor/graph/editor/EditorGraph.java
@@ -2,6 +2,7 @@ package com.lgsim.engine.graphEditor.graph.editor;
 
 import com.lgsim.engine.graphEditor.api.data.IGraph;
 import com.lgsim.engine.graphEditor.api.data.IVertex;
+import com.lgsim.engine.graphEditor.api.data.impl.VertexImpl;
 import com.mxgraph.model.mxCell;
 import com.mxgraph.model.mxGeometry;
 import com.mxgraph.util.mxEvent;
@@ -11,7 +12,9 @@ import org.slf4j.LoggerFactory;
 
 import java.awt.*;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 class EditorGraph extends mxGraph implements IGraph
 {
@@ -114,7 +117,86 @@ class EditorGraph extends mxGraph implements IGraph
     final Object defaultParent = getDefaultParent();
     final Object[] vertices = getChildVertices(defaultParent);
     final Object[] edges = getChildEdges(defaultParent);
+    final List<IVertex> output = new Vector<>();
+    for (Object vertex : vertices)
+    {
+      mxCell in = (mxCell) vertex;
+      IVertex v = (IVertex) in.getValue();
+      VertexImpl out = new VertexImpl();
+      cloneIfPossible(v, out);
+      List<IVertex> inputVertexes = lookupInputPorts(vertex, edges);
+      List<IVertex> outputVertexes = lookupOutputPorts(vertex, edges);
+      out.setInputPorts(inputVertexes);
+      out.setOutputPorts(outputVertexes);
+      output.add(out);
+    }
+    return output;
+  }
 
-    return null;
+
+  // TODO deep clone ？
+  private void cloneIfPossible(IVertex source, VertexImpl target)
+  {
+    target.setID(source.getID());
+    target.setTypeID(source.getTypeID());
+    target.setArguments(source.getArguments());
+    target.setOutputs(source.getOutputs());
+    target.setCavity(source.isCavity());
+  }
+
+
+  private List<IVertex> lookupInputPorts(Object vertex, Object[] edges)
+  {
+    List<IVertex> output = new Vector<>();
+    for (Object o : edges)
+    {
+      mxCell v = (mxCell) vertex;
+      mxCell edge = (mxCell) o;
+      if (notOrphanEdge(edge))
+      {
+        mxCell target = (mxCell) edge.getTarget();
+        if (cellEquals(v, target))
+        {
+          mxCell source = (mxCell) edge.getSource();
+          IVertex out = (IVertex) source.getValue();
+          output.add(out);
+        }
+      }
+    }
+    return output;
+  }
+
+
+  private List<IVertex> lookupOutputPorts(Object vertex, Object[] edges)
+  {
+    List<IVertex> output = new Vector<>();
+    for (Object o : edges)
+    {
+      mxCell v = (mxCell) vertex;
+      mxCell edge = (mxCell) o;
+      if (notOrphanEdge(edge))
+      {
+        mxCell source = (mxCell) edge.getSource();
+        if (cellEquals(v, source))
+        {
+          mxCell target = (mxCell) edge.getTarget();
+          IVertex out = (IVertex) target.getValue();
+          output.add(out);
+        }
+      }
+    }
+    return output;
+  }
+
+
+  private boolean cellEquals(mxCell x, mxCell y)
+  {
+    return x.getId().equals(y.getId());
+  }
+
+
+  private boolean notOrphanEdge(mxCell cell)
+  {
+    return (cell.getSource() != null) && (cell.getTarget() != null);
   }
 }
