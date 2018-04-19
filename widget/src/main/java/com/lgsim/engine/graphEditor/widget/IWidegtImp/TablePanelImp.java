7f02978b13ef6aa145924ea93da005ea003af020
yuyong <cx@lgsim.com>
2018-04-19 14:22:11 +0800

    0

--- widget/src/main/java/com/lgsim/engine/graphEditor/widget/IWidegtImp/TablePanelImp.java
+++ widget/src/main/java/com/lgsim/engine/graphEditor/widget/IWidegtImp/TablePanelImp.java
@@ -1,21 +1,30 @@
 package com.lgsim.engine.graphEditor.widget.IWidegtImp;
 
+import com.lgsim.engine.graphEditor.api.data.IVertex;
 import com.lgsim.engine.graphEditor.api.data.IVertexArgument;
 import com.lgsim.engine.graphEditor.api.widget.IWidget;
+import com.lgsim.engine.graphEditor.api.widget.table.IVertexTable;
 import com.lgsim.engine.graphEditor.widget.Component.TablePanel;
 import com.lgsim.engine.graphEditor.widget.PoJo.RowContent;
+import org.jetbrains.annotations.NotNull;
 
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TablePanelImp implements IWidget {
+public class TablePanelImp implements IVertexTable {
 
-    List<IVertexArgument> rowContents = new ArrayList<>();
+    TablePanel tablePanel = new TablePanel();
 
     @Override
     public JComponent getSwingComponent() {
-        return new TablePanel(rowContents);
+        return tablePanel;
+    }
+
+    @Override
+    public void render(@NotNull IVertex vertex) {
+        tablePanel.showTable(vertex);
     }
 }
