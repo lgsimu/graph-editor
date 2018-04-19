dd6d6c93bc40a080824d6e9e1ec66f57e2d37e54
leiqiao <leiqiao0@aliyun.com>
2018-04-19 17:30:18 +0800

    * fix issues

--- api/src/main/java/com/lgsim/engine/graphEditor/api/widget/IWidget.java
+++ api/src/main/java/com/lgsim/engine/graphEditor/api/widget/IWidget.java
@@ -1,5 +1,7 @@
 package com.lgsim.engine.graphEditor.api.widget;
 
+import org.jetbrains.annotations.NotNull;
+
 import javax.swing.*;
 
 public interface IWidget
@@ -7,5 +9,6 @@ public interface IWidget
   /**
    * 获取小部件的swing组件
    */
+  @NotNull
   JComponent getSwingComponent();
 }
