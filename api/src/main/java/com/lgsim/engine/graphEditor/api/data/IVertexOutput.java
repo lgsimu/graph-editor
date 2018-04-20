5f552afef907d0dd0240ff7713f52f5e09e3a217
FQ <1175896097@qq.com>
2018-04-20 18:01:04 +0800

    编写一个元件库的模板基类

--- api/src/main/java/com/lgsim/engine/graphEditor/api/data/IVertexOutput.java
+++ api/src/main/java/com/lgsim/engine/graphEditor/api/data/IVertexOutput.java
@@ -1,7 +1,35 @@
 package com.lgsim.engine.graphEditor.api.data;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Serializable;
 
 public interface IVertexOutput extends Serializable
 {
+    /**
+     * 参数id
+     */
+    @NotNull
+    String getID();
+
+
+    /**
+     * 单位
+     */
+    @NotNull
+    String getUnit();
+
+
+    /**
+     * 获取数值
+     */
+    double getValue();
+
+
+
+    /**
+     * 描述
+     */
+    @NotNull
+    String getDescription();
 }
