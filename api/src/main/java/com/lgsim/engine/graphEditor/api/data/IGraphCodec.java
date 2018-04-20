e2de0f472f5f02de75bc6e6061678977b5b84891
FQ <1175896097@qq.com>
2018-04-20 18:13:18 +0800

    编写注解和编码异常类

--- api/src/main/java/com/lgsim/engine/graphEditor/api/data/IGraphCodec.java
+++ api/src/main/java/com/lgsim/engine/graphEditor/api/data/IGraphCodec.java
@@ -8,14 +8,19 @@ import java.io.Serializable;
 
 public interface IGraphCodec
 {
-  /**
-   * 从图中读取信息，生成数据流
-   *
-   * @param graph 图数据模型
-   * @return 序列化对象
-   */
+    /**
+     * 从图中读取信息，生成数据流
+     * @param graph 图数据模型
+     * @return 序列化对象
+     * @throws EncodeException 编码异常
+     */
   @NotNull Serializable encode(@NotNull IGraph graph) throws EncodeException;
 
-
+    /**
+     * 解码后，生成输出流
+     * @param serializable 序列化对象
+     * @return os流
+     * @throws DecodeException 解码异常
+     */
   @NotNull IGraph decode(@NotNull Serializable serializable) throws DecodeException;
 }
--- api/src/main/java/com/lgsim/engine/graphEditor/api/data/impl/VertexOutputImpl.java
+++ api/src/main/java/com/lgsim/engine/graphEditor/api/data/impl/VertexOutputImpl.java
@@ -2,6 +2,6 @@ package com.lgsim.engine.graphEditor.api.data.impl;
 
 import com.lgsim.engine.graphEditor.api.data.IVertexOutput;
 
-public class VertexOutputImpl implements IVertexOutput
+/*public class VertexOutputImpl implements IVertexOutput
 {
-}
+}*/
