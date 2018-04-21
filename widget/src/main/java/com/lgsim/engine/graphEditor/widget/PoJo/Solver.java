b52e23e248fbcacbf902da65d56d19b93544c51f
yuyong <cx@lgsim.com>
2018-04-21 17:22:43 +0800

    catch some expection

--- widget/src/main/java/com/lgsim/engine/graphEditor/widget/PoJo/Solver.java
+++ widget/src/main/java/com/lgsim/engine/graphEditor/widget/PoJo/Solver.java
@@ -28,7 +28,12 @@ public class Solver {
         thread = new Thread() {
             public void run() {
                 File exeDir = environment.getExecutableFile();//获取求解器的目录
-                File inputFile = environment.getSolverInputFile();//获取文件的目录
+                File inputFile = null;//获取文件的目录
+                try {
+                    inputFile = environment.getSolverInputFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 String caseName = environment.getCaseName();//文件名称
                 String cmdArgument = environment.getSolverCommandlineArguments();//获取命令
                 String exeCmd = "LGSAS " + inputFile.toString() + "/" + caseName + " " + cmdArgument;
