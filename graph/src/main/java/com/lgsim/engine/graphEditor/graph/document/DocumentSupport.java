72f791a0e24b663f67bd49fbacc66d1eff81ae14
leiqiao <leiqiao0@aliyun.com>
2018-04-26 14:15:58 +0800

    * fix application action issues

--- graph/src/main/java/com/lgsim/engine/graphEditor/graph/document/DocumentSupport.java
+++ graph/src/main/java/com/lgsim/engine/graphEditor/graph/document/DocumentSupport.java
@@ -1,5 +1,7 @@
 package com.lgsim.engine.graphEditor.graph.document;
 
+import com.lgsim.engine.graphEditor.api.IApplication;
+import com.lgsim.engine.graphEditor.graph.action.ApplicationActionImpl;
 import com.lgsim.engine.graphEditor.graph.graph.Graph;
 import com.lgsim.engine.graphEditor.graph.graph.GraphComponent;
 import com.mxgraph.swing.mxGraphComponent;
@@ -12,12 +14,14 @@ import java.awt.*;
 public class DocumentSupport {
 
   @Contract(pure = true)
-  public static @NotNull Document createDocument(@NotNull DocumentContext context) {
+  public static @NotNull Document createDocument(@NotNull DocumentContext context, @NotNull IApplication application) {
     mxGraphComponent comp = new GraphComponent(new Graph());
     final Document document = new Document(context, comp);
     comp.setColumnHeaderView(new DocumentRuler(comp, DocumentRuler.ORIENTATION_HORIZONTAL));
     comp.setRowHeaderView(new DocumentRuler(comp, DocumentRuler.ORIENTATION_VERTICAL));
     document.getSwingComponent().setMinimumSize(new Dimension(320, 320));
+    ApplicationActionImpl action = new ApplicationActionImpl(document);
+    application.setApplicationAction(action);
     return document;
   }
 
