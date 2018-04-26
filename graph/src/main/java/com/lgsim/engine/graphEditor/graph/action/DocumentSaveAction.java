498b38044e60fd277a1098ae843d2429b67805ab
leiqiao <leiqiao0@aliyun.com>
2018-04-26 18:01:18 +0800

    * save document action

--- graph/src/main/java/com/lgsim/engine/graphEditor/graph/action/DocumentSaveAction.java
+++ graph/src/main/java/com/lgsim/engine/graphEditor/graph/action/DocumentSaveAction.java
@@ -5,23 +5,42 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.io.IOException;
 
 @SuppressWarnings("WeakerAccess")
 public class DocumentSaveAction extends DocumentAction {
+
   private static final Logger log = LoggerFactory.getLogger(DocumentSaveAction.class);
+  private JFileChooser saveLocationChooser;
+
+
   public DocumentSaveAction(@NotNull Document document) {
     super(document);
+    this.saveLocationChooser = new JFileChooser();
   }
+
 
   @Override
   public void actionPerformed(ActionEvent event)
   {
     log.debug("save document {}", document);
+    int code = saveLocationChooser.showOpenDialog(document.getSwingComponent());
+    if (code == JFileChooser.APPROVE_OPTION) {
+      File entry = saveLocationChooser.getSelectedFile();
+      save(entry);
+    }
+  }
+
+
+  private void save(@NotNull File entry) {
     try {
+      document.setEntryFilePath(entry.getPath());
       document.output();
-    } catch (IOException e) {
+    }
+    catch (IOException e) {
       log.debug("save document failed {}", e.getMessage());
     }
   }
