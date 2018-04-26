e1f0af9a48db5f4c51e6c6c8a7f02540b0150065
leiqiao <leiqiao0@aliyun.com>
2018-04-26 14:34:18 +0800

    * fix implementation util issues

---
+++ graph/src/main/java/com/lgsim/engine/graphEditor/graph/document/DocumentCodec.java
@@ -0,0 +1,20 @@
+package com.lgsim.engine.graphEditor.graph.document;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public class DocumentCodec {
+
+  @Contract(pure = true)
+  public static @NotNull byte[] encode(@NotNull Document document) {
+    // TODO 编码文档
+    return null;
+  }
+
+
+  @Contract(pure = true)
+  public static @NotNull Document decode(@NotNull byte[] bytes) {
+    // 解码 编码文档
+    return null;
+  }
+}
