ffe335d3a20ed07d649f9fced49a933dddc488b6
leiqiao <leiqiao0@aliyun.com>
2018-04-26 09:43:56 +0800

    * fix issues (with errors)

--- util/src/main/java/com/lgsim/engine/graphEditor/util/ImplementationUtil.java
+++ util/src/main/java/com/lgsim/engine/graphEditor/util/ImplementationUtil.java
@@ -1,6 +1,8 @@
 package com.lgsim.engine.graphEditor.util;
 
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -8,39 +10,37 @@ import java.util.Hashtable;
 import java.util.Map;
 
 public class ImplementationUtil {
-  private static final Map<Class<?>, Object> table = new Hashtable<>();
 
-  public static void putType(@NotNull Class<?> interfaceType, @NotNull Class<?> implType)
-  {
+  private static final Logger log = LoggerFactory.getLogger(ImplementationUtil.class);
+  private static final Map<Class<?>, Class<?>> table = new Hashtable<>();
+
+
+  public static void put(@NotNull Class<?> interfaceType, @NotNull Class<?> implType) {
+    log.debug("register type {} to {}", interfaceType.getName(), implType.getName());
     table.put(interfaceType, implType);
   }
 
-  public static <T> void putInstance(@NotNull Class<T> type, @NotNull T t) {
-    table.put(type, t);
-  }
 
   @NotNull
   @SuppressWarnings("unchecked")
   public static <T> T getInstanceOf(@NotNull Class<T> type) throws InstantiationException
   {
-    Object o = table.get(type);
-    if (o == null) {
-      throw new InstantiationException("cannot find any types or instances");
-    } else {
-      if (o instanceof Class<?>) {
-        try {
-          Class<?> implType = (Class<?>) o;
-          Constructor<?> cons = implType.getConstructor();
-          return (T) cons.newInstance();
-        } catch (InvocationTargetException e) {
-          String msg = e.getTargetException().getMessage();
-          throw new InstantiationException(msg);
-        } catch (IllegalAccessException | NoSuchMethodException e) {
-          throw new InstantiationException();
-        }
-      } else {
-        return (T) o;
+    try {
+      Class<?> implType = table.get(type);
+      if (implType != null) {
+        Constructor<?> cons = implType.getConstructor();
+        return (T) cons.newInstance();
       }
+      else {
+        throw new InstantiationException("unregister type " + type.getName());
+      }
+    }
+    catch (InvocationTargetException e) {
+      String msg = e.getTargetException().getMessage();
+      throw new InstantiationException(msg);
+    }
+    catch (IllegalAccessException | NoSuchMethodException e) {
+      throw new InstantiationException();
     }
   }
 }
