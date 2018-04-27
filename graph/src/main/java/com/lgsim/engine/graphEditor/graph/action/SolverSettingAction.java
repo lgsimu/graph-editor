053ab03fefc12aee4b348605ec0fabd0dc6a11f1
leiqiao <leiqiao0@aliyun.com>
2018-04-27 17:12:14 +0800

    * solver setting

--- graph/src/main/java/com/lgsim/engine/graphEditor/graph/action/SolverSettingAction.java
+++ graph/src/main/java/com/lgsim/engine/graphEditor/graph/action/SolverSettingAction.java
@@ -1,5 +1,6 @@
 package com.lgsim.engine.graphEditor.graph.action;
 
+import com.lgsim.engine.graphEditor.api.calc.SolverEnvironment;
 import com.lgsim.engine.graphEditor.graph.document.Document;
 import com.lgsim.engine.graphEditor.ui.SolverSettingsDialog;
 import org.jetbrains.annotations.NotNull;
@@ -7,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.event.ActionEvent;
+import java.io.File;
 
 @SuppressWarnings("WeakerAccess")
 public class SolverSettingAction extends SolverAction {
@@ -32,7 +34,11 @@ public class SolverSettingAction extends SolverAction {
   private void updateCalcEnv(@NotNull SolverSettingsDialog dialog) {
     String executable = dialog.getExecutable();
     String arguments = dialog.getArguments();
-    System.out.println(executable);
-    System.out.println(arguments);
+    SolverEnvironment env = new SolverEnvironment();
+    env.setExecutableFile(new File(executable));
+    env.setSolverCommandlineArguments(arguments);
+    env.setGraph(document.getGraph());
+    env.setCaseName(document.getTitle());
+    document.getApplication().setSolverEnvironment(env);
   }
 }
